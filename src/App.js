67930430a944ec9c17d55064e6e1952df6af848d
bobbidigi <bobbidigi34@gmail.com>
2020-02-13 18:32:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(){
+    super()
+    this.state = {
+      data: 'corgi',
+      images: []
+    }
+  }
+
+
+  handleChange = (event) => {
+    this.setState({
+      data: event.target.value
+    })
+  }
+
+  fetchDogImages = () => {
+    axios.get(`https://dog.ceo/api/breed/${this.state.data}/images/random/1`)
+    .then(res => {
+      console.log(res.data.message)
+      this.setState({
+        images: res.data.message
+      })
+    })
+  }
+  componentDidMount(){
+    this.fetchDogImages()
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.data !== this.state.data){
+      this.setState({
+        images: []
+      })
+      this.fetchDogImages()
+    }
+    // this.fetchDogImages()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>DOGGYS</h1>
+
+        <select value={this.state.data} onChange={this.handleChange}>
+          <option value="husky">Husky</option>
+          <option value="beagle">Beagle</option>
+          <option value="corgi">Corgi</option>
+        </select>
+
+      <div>
+        {this.state.images.map((image, index) =>{
+          return <img key={index} src={image} alt="dog"/>
+        })}
+      </div>
+      </div>
+    )
+  }
 }
-
-export default App;
