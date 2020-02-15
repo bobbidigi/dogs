b88809fd341271608315ecbe60a9d0fc46e3cb2a
bobbidigi <bobbidigi34@gmail.com>
2020-02-14 19:13:09 -0500

    converted to functional comp with hooks

--- src/App.js
+++ src/App.js
@@ -1,62 +1,43 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-export default class App extends Component {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-  constructor(){
-    super()
-    this.state = {
-      data: 'corgi',
-      images: []
-    }
-  }
+function App(props) {
+  const [data, setData] = useState([]);
+  const [breed, setBreed] = useState("husky");
 
+  const handleChange = event => {
+    setBreed(event.target.value);
+  };
 
-  handleChange = (event) => {
-    this.setState({
-      data: event.target.value
-    })
-  }
+  const fetchDogImages = () => {
+    axios
+      .get(`https://dog.ceo/api/breed/${breed}/images/random/1`)
+      .then(res => {
+        console.log(res.data.message);
+        setData(res.data.message);
+      });
+  };
 
-  fetchDogImages = () => {
-    axios.get(`https://dog.ceo/api/breed/${this.state.data}/images/random/1`)
-    .then(res => {
-      console.log(res.data.message)
-      this.setState({
-        images: res.data.message
-      })
-    })
-  }
-  componentDidMount(){
-    this.fetchDogImages()
-  }
+  useEffect(() => {
+    fetchDogImages();
+  }, [breed]);
 
-  componentDidUpdate(prevProps, prevState){
-    if(prevState.data !== this.state.data){
-      this.setState({
-        images: []
-      })
-      this.fetchDogImages()
-    }
-    // this.fetchDogImages()
-  }
+  return (
+    <div>
+      <h1>DOGGYS</h1>
 
-  render() {
-    return (
-      <div>
-        <h1>DOGGYS</h1>
-
-        <select value={this.state.data} onChange={this.handleChange}>
-          <option value="husky">Husky</option>
-          <option value="beagle">Beagle</option>
-          <option value="corgi">Corgi</option>
-        </select>
+      <select value={breed} onChange={handleChange}>
+        <option value="husky">Husky</option>
+        <option value="beagle">Beagle</option>
+        <option value="corgi">Corgi</option>
+      </select>
 
       <div>
-        {this.state.images.map((image, index) =>{
-          return <img key={index} src={image} alt="dog"/>
+        {data.map((image, index) => {
+          return <img key={index} src={image} alt="dog" />;
         })}
       </div>
-      </div>
-    )
-  }
+    </div>
+  );
 }
+export default App;
